2aad3db4df6317f1316faecefe411339b46f96a9
BookCnk <bookchanakarn@gmail.com>
2024-01-12 13:14:50 +0700

    fix variable

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.API_KEY,
   authDomain: import.meta.env.AUTH_DOMAIN,
   projectId: import.meta.env.PEOJECT_ID,
-  storageBucket: import.meta.env.MESSAGING_SENDER_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
   messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
   appId: import.meta.env.APP_ID,
   measurementId: import.meta.env.MEASUREMENT_ID,
